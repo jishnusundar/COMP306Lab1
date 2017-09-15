44b0c2452def0740f23c4f6c05bbf7a1c8e31cb9
= <jishnusundar423@gmail.com>
2017-09-15 17:33:29 -0400

    First commit

---
+++ Lab1 Hotel/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1_Hotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        
+    }
+}
